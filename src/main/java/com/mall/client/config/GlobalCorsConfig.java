641823e50d523147b12c262e12ea139c8aa71b50
twbin <a8343656@gmail.com>
2022-01-27 16:10:01 +0800

    設定跨域

---
+++ src/main/java/com/mall/client/config/GlobalCorsConfig.java
@@ -0,0 +1,18 @@
+package com.mall.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfig implements WebMvcConfigurer {
+ 
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")		//允許跨域的路徑
+                .allowedHeaders("*")	//允許請求的 header
+                .allowedMethods("*")	//允許的http方法
+                .maxAge(1800)
+                .allowedOrigins("http://localhost:8080"); //允許跨域的網域
+    }
+}
