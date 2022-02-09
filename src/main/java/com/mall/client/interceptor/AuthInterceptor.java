52175fc5bd7b4c02eea8d2b2c904cfb28b0d07eb
twbin <a8343656@gmail.com>
2022-02-09 15:17:05 +0800

    攔截器忽略 preflight request

--- src/main/java/com/mall/client/interceptor/AuthInterceptor.java
+++ src/main/java/com/mall/client/interceptor/AuthInterceptor.java
@@ -1,16 +1,23 @@
 package com.mall.client.interceptor;
 
+import java.io.PrintWriter;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mall.client.ErrorCode;
+import com.mall.client.dto.ActionResult;
 import com.mall.client.entity.UserLoginData;
 import com.mall.client.repository.UserLoginDataRepository;
 import com.sun.istack.Nullable;
@@ -25,11 +32,16 @@ public class AuthInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 	throws Exception {
+		
 		try {
+			// preflight request 略過檢查
+			if(request.getMethod().equals("OPTIONS")) {
+				return true;
+			}
 			
 			//檢查 userId 是否為空
-			if(request.getHeader("userId").isEmpty()) {
-				response.getWriter().write("userId is null");
+			if(StringUtils.isEmpty(request.getHeader("userId"))) {
+				System.out.println("userId 為空");
 				response.setStatus(401);
 				return false;
 			}
@@ -42,14 +54,14 @@ public class AuthInterceptor implements HandlerInterceptor{
 			
 			//token 無效
 			if(validLoginData.isEmpty()) {
-				response.getWriter().write("token expired");
-		        response.setStatus(401);
+				System.out.println("token 過期");
+				response.setStatus(401);
 				return false;
 			} else {
 				return true;
 			}
 		}catch(Exception ex) {
-			response.getWriter().write("server error plz check log");
+			System.out.println(ex);
 			response.setStatus(500);
 			return false;
 		}
