33091ce9529280dcbb8b53ff6444ebdeddb309ae
twbin <a8343656@gmail.com>
2022-02-21 15:23:08 +0800

    更正 entity 錯誤的 join 設定

--- src/main/java/com/mall/client/entity/BuylistDetail.java
+++ src/main/java/com/mall/client/entity/BuylistDetail.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -30,7 +31,7 @@ public class BuylistDetail extends NoVesionEntity{
 	@Column(name="product_id")
 	private Long productId;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
+	@OneToOne
 	@JoinColumn(name="product_id" ,insertable = false, updatable = false)
 	private Product product;
 	
